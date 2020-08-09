e7e9fdd4ba70d34b824db085a8965d95f4c574f0
Rinat Nadav <ringhost@gmail.com>
2020-08-09 22:23:33 +0000

    added a lodash _.memoize to make ffetch more effective

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 import musixmatch from '../apis/musixmatch';
-
+import _ from 'lodash';
 export const selectSong = (song)=>{
     return{
         type:'SONG_SELECTED',
@@ -10,9 +10,14 @@ export const fetchLyrics = ()=> async dispatch =>{
     const response = await musixmatch.get('/posts');
     dispatch({type:'FETCH_LYRICS',payload:response.data})               
 }
-export const fetchUser = (id)=> async dispatch =>{
+export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+
+const _fetchUser = _.memoize(async(id,dispatch)=>{
     const response = await musixmatch.get(`/users/${id}`);
     dispatch({type:'FETCH_USER',payload:response.data})               
-}
+})
+
+
+
 
 
